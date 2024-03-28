7db5bbaaece47646927e65d3d032af6767f9c4e0
Jimmy Tan <jimmytan0424@gmail.com>
2024-03-28 14:36:13 -0700

    Disable Nextjs caching in admin page

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,8 @@
 import { Nav, NavLink } from '@/components/Nav';
 
+// Disable Nextjs caching in admin page
+export const dynamic = 'force-dynamic';
+
 export default function AdminLayout({
   children,
 }: Readonly<{
