dae4edb2d7ca8d6ca200a88489793b19f7d36194
Jimmy Tan <jimmytan0424@gmail.com>
2024-04-04 15:22:19 -0700

    Set up endpoint for receiving webhook

---
+++ src/app/webhooks/stripe/route.tsx
@@ -0,0 +1,66 @@
+import db from '@/db/db';
+import { NextRequest, NextResponse } from 'next/server';
+import Stripe from 'stripe';
+import { Resend } from 'resend';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+const resend = new Resend(process.env.RESEND_API_KEY as string);
+
+export async function POST(req: NextRequest) {
+  const event = await stripe.webhooks.constructEvent(
+    await req.text(),
+    req.headers.get('stripe-signature') as string,
+    process.env.STRIPE_WEBHOOK_SECRET as string
+  );
+
+  // Handle the event
+  switch (event.type) {
+    //https://docs.stripe.com/api/events/types#event_types-charge.succeeded
+    case 'charge.succeeded':
+      const charge = event.data.object;
+      const productId = charge.metadata.productId;
+      const email = charge.billing_details.email;
+      const pricePaidInCents = charge.amount;
+
+      const product = await db.product.findUnique({ where: { id: productId } });
+      if (product == null || email == null) {
+        return new NextResponse('Bad Request', { status: 400 });
+      }
+
+      // Create a new user and add an order. If user exist, then add an order only
+      const userFields = {
+        email,
+        orders: { create: { productId, pricePaidInCents } },
+      };
+
+      const {
+        orders: [order],
+      } = await db.user.upsert({
+        where: { email },
+        create: userFields,
+        update: userFields,
+        select: { orders: { orderBy: { createdAt: 'desc' }, take: 1 } },
+      });
+
+      const downloadVerification = await db.downloadVerification.create({
+        data: {
+          productId,
+          expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24),
+        },
+      });
+
+      await resend.emails.send({
+        from: `Support <${process.env.SENDER_EMAIL}>`,
+        to: email,
+        subject: 'Order Confirmation',
+        react: <h1>Hello!</h1>,
+      });
+
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  return new NextResponse();
+}
