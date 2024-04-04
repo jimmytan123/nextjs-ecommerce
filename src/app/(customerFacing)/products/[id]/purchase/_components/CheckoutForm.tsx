8b720d26c312e2b40e33b870dc08013aba81d2e0
Jimmy Tan <jimmytan0424@gmail.com>
2024-04-04 10:36:03 -0700

    Allow purchase

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -16,13 +16,18 @@ import {
   useElements,
   useStripe,
   PaymentElement,
+  LinkAuthenticationElement,
 } from '@stripe/react-stripe-js';
 import {
   Appearance,
   StripeElementsOptions,
+  StripeLinkAuthenticationElementChangeEvent,
+  StripePaymentElementChangeEvent,
   loadStripe,
 } from '@stripe/stripe-js';
 import Image from 'next/image';
+import { FormEvent, useState } from 'react';
+import { userOrderExists } from '@/app/actions/orders';
 
 interface CheckoutFormProps {
   product: Product;
@@ -68,29 +73,136 @@ export default function CheckoutForm({
         </div>
       </div>
       <Elements options={options} stripe={stripePromise}>
-        <Form priceInCents={product.priceInCents} />
+        <Form priceInCents={product.priceInCents} productId={product.id} />
       </Elements>
     </div>
   );
 }
 
-function Form({ priceInCents }: { priceInCents: number }) {
+function Form({
+  priceInCents,
+  productId,
+}: {
+  priceInCents: number;
+  productId: string;
+}) {
   const stripe = useStripe();
   const elements = useElements();
 
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const [email, setEmail] = useState<string>();
+  const [isInputCompleted, setIsInputCompleted] = useState<boolean>(false);
+  const [isEmailCompleted, setIsEmailCompleted] = useState<boolean>(false);
+  const [isFormRendered, setIsFormRendered] = useState<boolean>(false);
+
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    if (!stripe || !elements || !email) {
+      // Stripe.js hasn't yet loaded or email field not provided
+      // Disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    setIsLoading(true);
+
+    // Check for existing order for the current user
+    const orderExists = await userOrderExists(email, productId);
+
+    if (orderExists) {
+      setErrorMessage(
+        'You have already purchased this product. Try downloading it from the My Orders page'
+      );
+      setIsLoading(false);
+      return;
+    }
+
+    const { error } = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        // Redirect URL after they complete the payment
+        return_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/stripe/purchase-success`,
+      },
+    });
+
+    if (error.type === 'card_error' || error.type === 'validation_error') {
+      setErrorMessage(error.message);
+    } else {
+      setErrorMessage('An unknown error occured');
+    }
+
+    setIsLoading(false);
+  }
+
+  function handleInputChange(event: StripePaymentElementChangeEvent) {
+    if (event.complete) {
+      setIsInputCompleted(true);
+    } else {
+      setIsInputCompleted(false);
+    }
+  }
+
+  function handleEmailChange(
+    event: StripeLinkAuthenticationElementChangeEvent
+  ) {
+    setEmail(event.value.email);
+    if (event.complete) {
+      setIsEmailCompleted(true);
+    } else {
+      setIsEmailCompleted(false);
+    }
+  }
+
   return (
-    <form action="">
+    <form onSubmit={handleSubmit}>
       <Card>
         <CardHeader>
           <CardTitle>Checkout</CardTitle>
-          <CardDescription className="text-destructive">Error</CardDescription>
+          {errorMessage && (
+            <CardDescription className="text-destructive">
+              {errorMessage}
+            </CardDescription>
+          )}
         </CardHeader>
         <CardContent>
-          <PaymentElement id="payment-element" />
+          {!isFormRendered && (
+            <div className="space-y-6 animate-pulse m-1">
+              <div className="flex gap-2 h-6">
+                <div className="w-3/5 h6 rounder bg-gray-100"></div>
+                <div className="w-2/5 h6 rounder bg-gray-100"></div>
+              </div>
+              <div className="flex gap-2 h-6">
+                <div className="w-1/2 h6 rounder bg-gray-100"></div>
+                <div className="w-1/2 h6 rounder bg-gray-100"></div>
+              </div>
+              <div className="w-full h-6 rounded bg-gray-100"></div>
+            </div>
+          )}
+          <PaymentElement
+            id="payment-element"
+            onChange={(e) => handleInputChange(e)}
+            onReady={() => setIsFormRendered(true)}
+          />
+          <div className="mt-4">
+            <LinkAuthenticationElement onChange={(e) => handleEmailChange(e)} />
+          </div>
         </CardContent>
         <CardFooter>
-          <Button className="w-full" size="lg" disabled={!stripe || !elements}>
-            Pay - {formatCurrency(priceInCents / 100)}
+          <Button
+            className="w-full"
+            size="lg"
+            disabled={
+              !stripe ||
+              !elements ||
+              isLoading ||
+              !isInputCompleted ||
+              !isEmailCompleted
+            }
+          >
+            {isLoading
+              ? 'Purchasing...'
+              : `Pay - ${formatCurrency(priceInCents / 100)}`}
           </Button>
         </CardFooter>
       </Card>
