9b20677a1dc13b021b35cd1868c8009b458752a1
Jimmy Tan <jimmytan0424@gmail.com>
2024-04-05 11:18:16 -0700

    add admin orders page

---
+++ src/app/admin/orders/_components/OrderActions.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { DropdownMenuItem } from '@/components/ui/dropdown-menu';
+import { startTransition, useTransition } from 'react';
+import { useRouter } from 'next/navigation';
+import { deleteOrder } from '../../_actions/orders';
+
+interface DeleteDropdownItemProps {
+  id: string;
+}
+export function DeleteDropdownItem({ id }: DeleteDropdownItemProps) {
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+
+  return (
+    <DropdownMenuItem
+      variant="destructive"
+      disabled={isPending}
+      onClick={() =>
+        startTransition(async () => {
+          await deleteOrder(id);
+          router.refresh();
+        })
+      }
+    >
+      Delete
+    </DropdownMenuItem>
+  );
+}
